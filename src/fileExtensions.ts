c3d14e631aa986d74cf66c80cf2b17be5e2bae90
Nuhman Pk <62880706+nuhmanpk@users.noreply.github.com>
2024-12-30 17:37:05 +0000

    fix: dart , php issue

--- src/fileExtensions.ts
+++ src/fileExtensions.ts
@@ -1,212 +1,147 @@
-import { getRandomEmoji, getRandomJoke } from './utlis';
-
+import { getRandomEmoji, getRandomJoke } from './utils';
 
 export const fileExtensions = (fileExtension: string, filePath?: string, selectedText?: string) => {
-    const defaultPrintStatement = `"${getRandomEmoji()} ~ ${filePath} -> ${selectedText}: ",  ${selectedText ? selectedText : `"${getRandomJoke()}"`}`;
-    const language: any =
-    {
+    const defaultPrintStatement = `${getRandomEmoji()} ~ ${filePath || 'Unknown file'} -> ${selectedText || getRandomJoke()}`;
+    
+    const dartPrintStatement = selectedText
+        ? selectedText.includes(".")
+            ? `${getRandomEmoji()} ${filePath || 'Unknown file'} -> ${selectedText}: \$\{${selectedText}\}`
+            : `${getRandomEmoji()} ${filePath || 'Unknown file'} -> ${selectedText}: \$${selectedText}`
+        : `${getRandomEmoji()} ${filePath || 'Unknown file'}`;
+
+    const language: Record<string, { language: string; printSyntax: string; commentSyntax: string }> = {
         'js': {
             language: 'JavaScript',
-            printSyntax: `console.log(${defaultPrintStatement});`,
+            printSyntax: `console.log('${defaultPrintStatement}');`,
             commentSyntax: "//",
         },
         'ts': {
             language: 'TypeScript',
-            printSyntax: `console.log(${defaultPrintStatement});`,
+            printSyntax: `console.log('${defaultPrintStatement}');`,
             commentSyntax: "//",
         },
         'tsx': {
             language: 'TypeScript (JSX)',
-            printSyntax: `console.log(${defaultPrintStatement});`,
+            printSyntax: `console.log('${defaultPrintStatement}');`,
             commentSyntax: "//",
         },
         'jsx': {
             language: 'JavaScript (JSX)',
-            printSyntax: `console.log(${defaultPrintStatement});`,
+            printSyntax: `console.log('${defaultPrintStatement}');`,
             commentSyntax: "//",
         },
         'vue': {
             language: 'Vue.js',
-            printSyntax: `console.log(${defaultPrintStatement});`,
+            printSyntax: `console.log("${defaultPrintStatement}");`,
             commentSyntax: "//",
         },
         'svelte': {
             language: 'Svelte',
-            printSyntax: `console.log(${defaultPrintStatement});`,
+            printSyntax: `console.log("${defaultPrintStatement}");`,
             commentSyntax: "//",
         },
         'py': {
             language: 'Python',
-            printSyntax: `print(${defaultPrintStatement})`,
+            printSyntax: `print("${defaultPrintStatement}")`,
             commentSyntax: "#",
         },
         'ipynb': {
-            language: 'Jupyter Notebbok',
-            printSyntax: `print(${defaultPrintStatement})`,
+            language: 'Jupyter Notebook',
+            printSyntax: `print("${defaultPrintStatement}")`,
             commentSyntax: "#",
         },
         'java': {
             language: 'Java',
-            printSyntax: `System.out.println(${defaultPrintStatement});`,
+            printSyntax: `System.out.println("${defaultPrintStatement}");`,
             commentSyntax: "//",
         },
         'c': {
             language: 'C',
-            printSyntax: `printf(${defaultPrintStatement});`,
+            printSyntax: `printf("${defaultPrintStatement}\\n");`,
             commentSyntax: "//",
         },
         'cpp': {
             language: 'C++',
-            printSyntax: `std::cout << ${defaultPrintStatement} << std::endl;`,
+            printSyntax: `std::cout << "${defaultPrintStatement}" << std::endl;`,
             commentSyntax: "//",
         },
         // eslint-disable-next-line @typescript-eslint/naming-convention
         'c#': {
             language: 'C#',
-            printSyntax: `Console.WriteLine(${defaultPrintStatement});`,
+            printSyntax: `Console.WriteLine("${defaultPrintStatement}");`,
             commentSyntax: "//",
         },
         'rb': {
             language: 'Ruby',
-            printSyntax: `puts ${defaultPrintStatement};`,
+            printSyntax: `puts "${defaultPrintStatement}"`,
             commentSyntax: "#",
         },
         'php': {
             language: 'PHP',
-            printSyntax: `echo (${defaultPrintStatement});`,
+            printSyntax: `echo "${getRandomEmoji()} ~ ${filePath} -> \$${selectedText}: " . (${selectedText ? `print_r(${selectedText}, true)` : `"${getRandomJoke()}"`});`,
             commentSyntax: "//",
         },
         'go': {
             language: 'Go',
-            printSyntax: `fmt.Println(${defaultPrintStatement})`,
+            printSyntax: `fmt.Println("${defaultPrintStatement}")`,
             commentSyntax: "//",
         },
         'swift': {
             language: 'Swift',
-            printSyntax: `print(${defaultPrintStatement})`,
+            printSyntax: `print("${defaultPrintStatement}")`,
             commentSyntax: "//",
         },
         'kotlin': {
             language: 'Kotlin',
-            printSyntax: `println(${defaultPrintStatement})`,
+            printSyntax: `println("${defaultPrintStatement}")`,
             commentSyntax: "//",
         },
         'rust': {
             language: 'Rust',
-            printSyntax: `println!(${defaultPrintStatement});`,
+            printSyntax: `println!("${defaultPrintStatement}");`,
             commentSyntax: "//",
         },
         'perl': {
             language: 'Perl',
-            printSyntax: `print ${defaultPrintStatement};`,
+            printSyntax: `print "${defaultPrintStatement}";`,
             commentSyntax: "#",
         },
         'r': {
             language: 'R',
-            printSyntax: `cat(${defaultPrintStatement})`,
+            printSyntax: `cat("${defaultPrintStatement}\\n")`,
             commentSyntax: "#",
         },
         'dart': {
             language: 'Dart',
-            printSyntax: `print(${defaultPrintStatement});`,
+            printSyntax: `print("${dartPrintStatement}");`,
             commentSyntax: "//",
         },
         'scala': {
             language: 'Scala',
-            printSyntax: `println(${defaultPrintStatement});`,
+            printSyntax: `println("${defaultPrintStatement}")`,
             commentSyntax: "//",
         },
         'lua': {
             language: 'Lua',
-            printSyntax: `print(${defaultPrintStatement})`,
+            printSyntax: `print("${defaultPrintStatement}")`,
             commentSyntax: "--",
-        },
-        'perl6': {
-            language: 'Perl 6',
-            printSyntax: `say ${defaultPrintStatement};`,
-            commentSyntax: "#",
-        },
-        'haskell': {
-            language: 'Haskell',
-            printSyntax: `putStrLn ${defaultPrintStatement}`,
-            commentSyntax: "--",
-        },
-        'racket': {
-            language: 'Racket',
-            printSyntax: `(displayln (format ${defaultPrintStatement}))`,
-            commentSyntax: ";",
         },
         'elixir': {
             language: 'Elixir',
-            printSyntax: `IO.puts(${defaultPrintStatement})`,
+            printSyntax: `IO.puts("${defaultPrintStatement}")`,
             commentSyntax: "#",
         },
         'julia': {
             language: 'Julia',
-            printSyntax: `println(${defaultPrintStatement})`,
-            commentSyntax: "#",
-        },
-        'groovy': {
-            language: 'Groovy',
-            printSyntax: `println(${defaultPrintStatement})`,
-            commentSyntax: "//",
-        },
-        'cobol': {
-            language: 'COBOL',
-            printSyntax: `DISPLAY ${defaultPrintStatement}"`,
-            commentSyntax: "*>",
-        },
-        'fortran': {
-            language: 'Fortran',
-            printSyntax: `WRITE(*,*) ${defaultPrintStatement}"`,
-            commentSyntax: "!",
-        },
-        'vhdl': {
-            language: 'VHDL',
-            printSyntax: `report ${defaultPrintStatement}`,
-            commentSyntax: "--",
-        },
-        'verilog': {
-            language: 'Verilog',
-            printSyntax: `$display(${defaultPrintStatement})`,
-            commentSyntax: "//",
-        },
-        'pascal': {
-            language: 'Pascal',
-            printSyntax: `WriteLn(${defaultPrintStatement});`,
-            commentSyntax: "//",
-        },
-        'ada': {
-            language: 'Ada',
-            printSyntax: `Put_Line(${defaultPrintStatement});`,
-            commentSyntax: "--",
-        },
-        'sql': {
-            language: 'SQL',
-            printSyntax: `PRINT ${defaultPrintStatement};`,
-            commentSyntax: "--",
-        },
-        'lisp': {
-            language: 'Lisp',
-            printSyntax: `(format t ${defaultPrintStatement})`,
-            commentSyntax: ";;",
-        },
-        'scheme': {
-            language: 'Scheme',
-            printSyntax: `(display (format ${defaultPrintStatement}))`,
-            commentSyntax: ";",
-        },
-        'powershell': {
-            language: 'PowerShell',
-            printSyntax: `Write-Host ${defaultPrintStatement})`,
+            printSyntax: `println("${defaultPrintStatement}")`,
             commentSyntax: "#",
         },
     };
 
-    try {
-        return language[fileExtension];
-    } catch (error) {
-        console.log(error);
+    if (!language[fileExtension]) {
+        console.error(`Unsupported file extension: ${fileExtension}`);
+        return null;
     }
 
+    return language[fileExtension];
 };
