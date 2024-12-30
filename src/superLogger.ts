b802d8ad65f353bd32495c4d404ec56dd7d78edd
Nuhman Pk <62880706+nuhmanpk@users.noreply.github.com>
2024-12-30 17:37:22 +0000

    typo: correction on file name

--- src/superLogger.ts
+++ src/superLogger.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
-import { formatFile, calculateIndentation, getRandomEmoji } from './utlis';
+import { formatFile, calculateIndentation, getRandomEmoji } from './utils';
 import { fileExtensions } from './fileExtensions';
 
 
